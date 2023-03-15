e5082d103b0cf4e7fbf82b0a987d55b6e997523c
Andrei Ignatov <super.dronshamann@gmail.com>
2023-03-15 19:50:56 +0400

    3 commit

--- mine.py
+++ mine.py
@@ -1,12 +1,11 @@
-class Dog(object):
-    def nickname(self, name):
-        self.name = name
-
-dog1 = Dog()
-bobik = dog1.nickname("Bobik")
-print(dog1.name)
-# Bobik
-print(bobik)
-
-
-
+# put your python code here
+# put your python code here
+n = int(input())
+net = []
+i = 0
+for i in range(0, n):
+    a = input()
+    for z in range(0, len(a)):
+        l = a[z]
+        net.append(l)
+print(net)
